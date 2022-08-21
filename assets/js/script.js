96114af08db54d8a0a6c45106636c8caea708752
Hannah Alverson <alverson98@yahoo.com>
2022-08-21 01:39:02 -0400

    added forecast function & changed data into an object

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,20 +5,29 @@ $(document).ready(function () {
   var cityInput = $("#city-input");
   var searchHistory = $("#search-history");
   var currentWeather = $("#current-weather");
-  var forecast = $("#forecast");
+  var forecastWeather = $("#forecast");
+  var submitBtn = $("#submit-btn");
 
   // Search variables
-  var cityName = "Chicago";
+  var cityName = "";
   var cityHistory = [];
 
   // API - weatherbit.io
-  //current weather
   var currentWeatherURL =
     "https://api.weatherbit.io/v2.0/current?city=" +
     cityName +
-    "&key=020dba26b42c4d61b7324a5ea43152c7&units=I";
-  getCurrentWeather();
+    "&country=US&key=020dba26b42c4d61b7324a5ea43152c7&units=I";
 
+  var forecastWeatherURL =
+    "https://api.weatherbit.io/v2.0/forecast/daily?city=" +
+    cityName +
+    "&country=US&key=020dba26b42c4d61b7324a5ea43152c7&units=I&days=5";
+
+  // Calling functions for API calls
+  getCurrentWeather();
+  getForecastWeather();
+
+  // Current Weather Data
   function getCurrentWeather() {
     fetch(currentWeatherURL)
       .then(function (response) {
@@ -26,14 +35,47 @@ $(document).ready(function () {
       })
       // setting variables of data from API
       .then(function (data) {
-        var city = data.data[0].city_name;
-        var date = data.data[0].datetime;
-        var weatherIcon = data.data[0].weather.icon;
-        var weatherStatus = data.data[0].weather.description;
-        var temp = data.data[0].temp;
-        var humidity = data.data[0].rh;
-        var windSpeed = data.data[0].wind_spd;
-        var uv = data.data[0].uv;
+        var current = {
+          city: data.data[0].city_name,
+          date: data.data[0].datetime,
+          weatherIcon: data.data[0].weather.icon,
+          weatherStatus: data.data[0].weather.description,
+          temp: data.data[0].temp,
+          humidity: data.data[0].rh,
+          windSpeed: data.data[0].wind_spd,
+          uv: data.data[1].uv,
+        };
       });
   }
+
+  // Forecasted Weather
+  function getForecastWeather() {
+    fetch(forecastWeatherURL)
+      .then(function (response) {
+        return response.json();
+      })
+      // setting variables of data from API
+      .then(function (data) {
+        var forecast = {
+          date: data.data[0].datetime,
+          weatherIcon: data.data[0].weather.icon,
+          weatherStatus: data.data[0].weather.description,
+          temp: data.data[0].temp,
+          humidity: data.data[0].rh,
+          windSpeed: data.data[0].wind_spd,
+        };
+      });
+  }
+
+  // Submitting City - new input or history
+  $(submitBtn, cityBtn).on("click", function (event) {
+    event.preventDefault;
+    var clickEvent = $(event.target);
+    // new city vs. city history select
+    if (clickEvent.id === submitBtn) {
+      var chosenCity = $(cityInput).val().trim();
+    } else {
+      var chosenCity = clickEvent.innerText;
+    }
+  });
 });
